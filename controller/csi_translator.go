ba6fc4c08373f3d030cd72f98517624f834867df
Tomas Vik <me@viktomas.com>
2023-09-27 15:37:59 +0200

    implement SGR color support

--- controller/csi_translator.go
+++ controller/csi_translator.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"slices"
 
 	"github.com/viktomas/gritty/buffer"
 	"github.com/viktomas/gritty/parser"
@@ -141,8 +140,33 @@ func translateCSI(op parser.Operation, b *buffer.Buffer, pty io.Writer) {
 		// SGR https://vt100.net/docs/vt510-rm/SGR.html
 	case 'm':
 		ps := op.Param(0, 0)
-		if !slices.Contains([]int{0, 1, 7, 27}, ps) {
-			log.Printf("unknown SGR instruction %v\n", op)
+		// 4bit color
+		if ps >= 30 && ps <= 37 {
+			color := get3bitNormalColor(uint8(ps - 30))
+			br := b.Brush()
+			br.FG = color
+			b.SetBrush(br)
+			return
+		}
+		if ps >= 90 && ps <= 97 {
+			color := get3bitBrightColor(uint8(ps - 90))
+			br := b.Brush()
+			br.FG = color
+			b.SetBrush(br)
+			return
+		}
+		if ps >= 40 && ps <= 47 {
+			color := get3bitNormalColor(uint8(ps - 40))
+			br := b.Brush()
+			br.BG = color
+			b.SetBrush(br)
+			return
+		}
+		if ps >= 100 && ps <= 107 {
+			color := get3bitNormalColor(uint8(ps - 100))
+			br := b.Brush()
+			br.BG = color
+			b.SetBrush(br)
 			return
 		}
 		switch ps {
@@ -160,8 +184,101 @@ func translateCSI(op parser.Operation, b *buffer.Buffer, pty io.Writer) {
 			br := b.Brush()
 			br.Invert = false
 			b.SetBrush(br)
+		case 38:
+			switch op.Param(1, 0) {
+			case 5:
+				color := get256Color(uint8(op.Param(2, 0)))
+				br := b.Brush()
+				br.FG = color
+				b.SetBrush(br)
+			case 2:
+				color := buffer.NewColor(uint8(op.Param(2, 0)), uint8(op.Param(3, 0)), uint8(op.Param(4, 0)))
+				br := b.Brush()
+				br.FG = color
+				b.SetBrush(br)
+
+			default:
+				log.Printf("unknown SGR instruction %v\n", op)
+			}
+		case 48:
+			switch op.Param(1, 0) {
+			case 5:
+				color := get256Color(uint8(op.Param(2, 0)))
+				br := b.Brush()
+				br.BG = color
+				b.SetBrush(br)
+			case 2:
+				color := buffer.NewColor(uint8(op.Param(2, 0)), uint8(op.Param(3, 0)), uint8(op.Param(4, 0)))
+				br := b.Brush()
+				br.BG = color
+				b.SetBrush(br)
+			default:
+				log.Printf("unknown SGR instruction %v\n", op)
+			}
+		default:
+			log.Printf("unknown SGR instruction %v\n", op)
 		}
 	default:
 		log.Printf("Unknown CSI instruction %v", op)
+	}
+}
+
+// get3bitNormalColor returns a color based on the SGR 30-37
+// I used the VS Code color palette
+// more info here https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
+func get3bitNormalColor(n uint8) buffer.Color {
+	if n > 7 {
+		log.Printf("get3BitNormalColor was invoked with incorrect number %d\n", n)
+		n = 0
+	}
+	colors := []buffer.Color{
+		{R: 0, G: 0, B: 0},       // black
+		{R: 205, G: 49, B: 49},   // red
+		{R: 13, G: 188, B: 121},  // green
+		{R: 229, G: 229, B: 16},  // yellow
+		{R: 36, G: 114, B: 200},  // blue
+		{R: 188, G: 63, B: 188},  // magenta
+		{R: 17, G: 168, B: 205},  // cyan
+		{R: 229, G: 229, B: 229}, // white
+	}
+	return colors[n]
+}
+
+// get3bitBrightColor returns a color based on the SGR 30-37 and 90-97 scale
+// I used the VS Code color palette
+// more info here https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit
+func get3bitBrightColor(n uint8) buffer.Color {
+	if n > 7 {
+		log.Printf("get3BitBrightColor was invoked with incorrect number %d\n", n)
+		n = 0
+	}
+	colors := []buffer.Color{
+		{R: 102, G: 102, B: 102}, // bright black
+		{R: 241, G: 76, B: 76},   // bright red
+		{R: 35, G: 209, B: 139},  // bright green
+		{R: 245, G: 245, B: 67},  // bright yellow
+		{R: 59, G: 142, B: 234},  // bright blue
+		{R: 214, G: 112, B: 214}, // bright magenta
+		{R: 41, G: 184, B: 219},  // bright cyan
+		{R: 229, G: 229, B: 229}, // bright white
+	}
+	return colors[n]
+}
+
+func get256Color(n uint8) buffer.Color {
+	switch {
+	case n < 8:
+		return get3bitNormalColor(n)
+	case n < 16:
+		return get3bitBrightColor(n - 8)
+	case n < 232:
+		// 6x6x6 color cube
+		n -= 16
+		levels := []uint8{0, 95, 135, 175, 215, 255}
+		return buffer.NewColor(levels[(n/36)%6], levels[(n/6)%6], levels[n%6])
+	default:
+		// grayscale
+		level := 8 + (n-232)*10
+		return buffer.NewColor(level, level, level)
 	}
 }
